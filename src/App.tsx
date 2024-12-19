045ec155320644dd4afd05d37406d1a312972806
xebec19 <rohant946@gmail.com>
2024-12-19 22:21:34 +0530

    Auto-commit: Thursday 19 December 2024 10:21:34 PM IST

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import "./App.css";
 import web3 from "./web3";
-import { useEffect } from "react";
-// import Web3 from "web3";
+import { useEffect, useState } from "react";
+import lottery from "./lottery";
+import Web3 from "web3";
 
-// window.ethereum.request({
-//   method: "eth_requestAccounts",
-// });
+window.ethereum.request({
+  method: "eth_requestAccounts",
+});
 
 function App() {
+  const [manager, setManager] = useState<string>("");
+
   useEffect(() => {
-    web3.eth.getAccounts().then(console.log);
+    async function fetchAccount() {
+      web3.eth.getAccounts().then(console.log);
+      const managerAddr = await lottery.methods.manager().call();
+      setManager(managerAddr + "");
+    }
+
+    fetchAccount();
   }, []);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="prose lg:prose-xl">
+      <h1>Lottery Contract</h1>
+      <h1>This contract is managed by {manager}</h1>
+    </div>
   );
 }
 
