aec3e34aee7e4267c7c02c72b12226bea990a2b3
xebec19 <rohant946@gmail.com>
2024-12-30 22:05:38 +0530

    Auto-commit: Monday 30 December 2024 10:05:38 PM IST

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
   const [manager, setManager] = useState<string>("");
   const [players, setPlayers] = useState<string[]>([]);
   const [balance, setBalance] = useState<string>("");
+  const [value, setValue] = useState<string>("");
 
   async function fetchPlayers() {
     const playersList = (await lottery.methods.getPlayers().call()) as string[];
@@ -19,9 +20,29 @@ function App() {
   }
 
   async function getBalance() {
-    const bal = (await web3.eth.getBalance(lottery.options.address)) as string;
+    const bal = (await web3.eth.getBalance(lottery.options.address!)) + "";
     setBalance(bal);
   }
+
+  const onSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, "ether"),
+    });
+  };
+
+  const onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+
+    // this.setState({ message: "A winner has been picked!" });
+  };
 
   useEffect(() => {
     async function fetchAccount() {
@@ -32,6 +53,7 @@ function App() {
 
     fetchAccount();
     fetchPlayers();
+    getBalance();
   }, []);
 
   return (
@@ -42,6 +64,16 @@ function App() {
         There are currently {players.length} players entered, competing to win{" "}
         {web3.utils.fromWei(balance, "ether")} ether!
       </h2>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="value">Enter a value in ether</label>
+        <input
+          type="text"
+          id="value"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
